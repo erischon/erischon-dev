87695eeba71804e6d05e0e99a606e60177fa3a3e
Eri Schon <erischon@gmail.com>
2022-01-22 18:29:04 +0100

    added cors

--- app/config/settings.py
+++ app/config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     # 3rd
     "tinymce",
 	'rest_framework',
+    'corsheaders',
     # Local
     "blog",
 ]
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -131,3 +133,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+    'http://localhost:8000',
+)
